fcc70d9c68ddcd8935f2a6ffdeedc6953c59105e
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-09-03 18:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function highlight() {
-    //Write your code here
+ function highlight() {
+            //Write your code here
+            const strongElements = document.getElementsByTagName('strong');
+            for (let i = 0; i < strongElements.length; i++) {
+                strongElements[i].style.backgroundColor = 'yellow';
 
+            }
+        }
 
-}
+        function return_normal() {
+            //Write your code here
 
-
-function return_normal() {
-    //Write your code here
-
-    
-}
+            const strongElements = document.getElementsByTagName('strong');
+            for (let i = 0; i < strongElements.length; i++) {
+                strongElements[i].style.backgroundColor = 'transparent';
+            }
+        }
